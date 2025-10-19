97e8577e7b99d08c285cf6bf0742e13003eed1b3
AMIN <a.jamalkhah@gmail.com>
2025-10-19 12:46:08 -0700

    FSM header file

---
+++ include/fsm.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* outputs */
+typedef struct {
+    float pump_duty; /* 0..100 (%) */
+    float fan_duty;  /* 0..100 (%) */
+} Actuators;
+
+/* system states */
+typedef enum {
+    ST_OFF = 0,
+    ST_IDLE,
+    ST_COOLING,
+    ST_FAULT
+} FsmState;
+
+/* FSM control block */
+typedef struct {
+    FsmState state;
+    int bad_sensor; /* fault flag */
+} Fsm;
+
+/* init and step */
+void fsm_init(Fsm* f);
+void fsm_step(Fsm* f, int ignition_on, float temp_c, float setpoint_c,
+              float pid_out, float fan_on_delta_c, int sensor_ok,
+              Actuators* act);
+
+#ifdef __cplusplus
+}
+#endif
+
